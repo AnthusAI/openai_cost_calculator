8519c11b7321cc5c9194e3cf8801337b46581fbc
Ryan Alyn Porter <rap@endymion.com>
2024-03-06 18:07:13 -0500

    Added `gpt-3.5-turbo-preview`.

--- openai_cost_calculator/pricing_information.py
+++ openai_cost_calculator/pricing_information.py
@@ -9,6 +9,7 @@ model_pricing = {
     'gpt-3.5-turbo-0125': {'input_price': 0.50, 'output_price': 1.50},
     'gpt-3.5-turbo-instruct': {'input_price': 1.50, 'output_price': 2.00},
     'gpt-3.5-turbo-1106': {'input_price': 1.00, 'output_price': 2.00},
+    'gpt-3.5-turbo-preview': {'input_price': 1.00, 'output_price': 2.00},
     'gpt-3.5-turbo-0613': {'input_price': 1.50, 'output_price': 2.00},
     'gpt-3.5-turbo-16k-0613': {'input_price': 3.00, 'output_price': 4.00},
     'gpt-3.5-turbo-0301': {'input_price': 1.50, 'output_price': 2.00},
