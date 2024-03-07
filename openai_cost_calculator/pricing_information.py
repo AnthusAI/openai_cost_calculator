3b3dc89291bb533f1ee2c5ddeb40776cb7dbba34
Ryan Alyn Porter <rap@endymion.com>
2024-03-06 19:05:18 -0500

    Fixed `gpt-4-turbo-preview`.

--- openai_cost_calculator/pricing_information.py
+++ openai_cost_calculator/pricing_information.py
@@ -2,6 +2,7 @@
 # Prices are per million tokens.
 model_pricing = {
     'gpt-4-0125-preview': {'input_price': 10.00, 'output_price': 30.00},
+    'gpt-4-turbo-preview': {'input_price': 10.00, 'output_price': 30.00},
     'gpt-4-1106-preview': {'input_price': 10.00, 'output_price': 30.00},
     'gpt-4-1106-vision-preview': {'input_price': 10.00, 'output_price': 30.00},
     'gpt-4': {'input_price': 30.00, 'output_price': 60.00},
@@ -9,7 +10,6 @@ model_pricing = {
     'gpt-3.5-turbo-0125': {'input_price': 0.50, 'output_price': 1.50},
     'gpt-3.5-turbo-instruct': {'input_price': 1.50, 'output_price': 2.00},
     'gpt-3.5-turbo-1106': {'input_price': 1.00, 'output_price': 2.00},
-    'gpt-3.5-turbo-preview': {'input_price': 1.00, 'output_price': 2.00},
     'gpt-3.5-turbo-0613': {'input_price': 1.50, 'output_price': 2.00},
     'gpt-3.5-turbo-16k-0613': {'input_price': 3.00, 'output_price': 4.00},
     'gpt-3.5-turbo-0301': {'input_price': 1.50, 'output_price': 2.00},
