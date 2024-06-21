1c3a5f3dc8cac98d8fff8ebfe66d0a7bfe7298f3
Derek Norrbom <dereknorrbom@gmail.com>
2024-06-21 17:11:30 +0000

    New model pricing

--- openai_cost_calculator/pricing_information.py
+++ openai_cost_calculator/pricing_information.py
@@ -13,5 +13,7 @@ model_pricing = {
     'gpt-3.5-turbo-0613': {'input_price': 1.50, 'output_price': 2.00},
     'gpt-3.5-turbo-16k-0613': {'input_price': 3.00, 'output_price': 4.00},
     'gpt-3.5-turbo-0301': {'input_price': 1.50, 'output_price': 2.00},
-    'gemini-1.5-flash-preview-0514': {'input_price': .50, 'output_price': 1.50}
+    'gemini-1.5-flash-preview-0514': {'input_price': 0.50, 'output_price': 1.50},
+    'claude-3-haiku-20240307-v1': {'input_price': 0.25, 'output_price': 1.25},
+    'meta.llama3-8b-instruct-v1': {'input_price': 0.40, 'output_price': 0.60},
 }
