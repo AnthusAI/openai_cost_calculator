b7f4436584aac126de089614a6ed81ea7bd13675
SchadenKai <kairustecsonpersonal@gmail.com>
2026-01-18 15:58:41 +0800

    feat: migrate the whole project from pip to uv; update gitignore to ignore .venv dir

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='openai_cost_calculator',
-    version='0.1.0',
+    name="openai_cost_calculator",
+    version="0.1.0",
     packages=find_packages(),
     # rest of your setup.py content
 )
